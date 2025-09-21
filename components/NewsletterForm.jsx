2427167078b2de648e9f531bf32e5dce212f0b0b
Amansh1824 <166283785+Amansh1824@users.noreply.github.com>
2025-09-21 22:19:41 +0530

    Update NewsletterForm.jsx

--- components/NewsletterForm.jsx
+++ components/NewsletterForm.jsx
@@ -12,7 +12,7 @@ export default function NewsletterForm() {
 
     try {
       const res = await fetch(
-        " https://buttondown.com/as8947007", // <-- CHANGE THIS
+        "https://buttondown.com/refer/as8947007", // <-- CHANGE THIS
         {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
