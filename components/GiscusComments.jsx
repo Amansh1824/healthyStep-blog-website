9a8fa1c14d67c5e11e387f957b875adce4cff258
Amansh1824 <166283785+Amansh1824@users.noreply.github.com>
2025-09-21 14:34:17 +0530

    Update GiscusComments.jsx

--- components/GiscusComments.jsx
+++ components/GiscusComments.jsx
@@ -1,5 +1,8 @@
 'use client'
-import Giscus from "@giscus/react";
+//import Giscus from "@giscus/react";
+//export default function GiscusComments(){
+  //return (<Giscus repo="YOUR_GITHUB_USER/YOUR_REPO" repoId="REPO_ID" category="General" categoryId="CATEGORY_ID" mapping="pathname" reactionsEnabled="1" emitMetadata="0" inputPosition="bottom" theme="preferred_color_scheme" lang="en" loading="lazy"/>);
+//}
 export default function GiscusComments(){
-  return (<Giscus repo="YOUR_GITHUB_USER/YOUR_REPO" repoId="REPO_ID" category="General" categoryId="CATEGORY_ID" mapping="pathname" reactionsEnabled="1" emitMetadata="0" inputPosition="bottom" theme="preferred_color_scheme" lang="en" loading="lazy"/>);
+  return null;
 }
